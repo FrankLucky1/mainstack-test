61eed19f6cd31152c50e9a6bf91630e0b4a0f677
FrankLucky1 <franklucky0@gmail.com>
2025-11-07 17:04:29 +0100

    bug-fix: cleaning code for deployment

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { Provider } from "react-redux";
-import { store } from "./store.ts";
 import "react-datepicker/dist/react-datepicker.css";
+import { ReduxProvider } from "./providers/ReduxProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider store={store}>
+    <ReduxProvider>
       <App />
-    </Provider>
+    </ReduxProvider>
   </StrictMode>
 );
 
